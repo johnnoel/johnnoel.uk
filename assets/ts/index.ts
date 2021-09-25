6bbf7fc464365d56bef6171535a22c3cb12306fd
John Noel <chaostangent@gmail.com>
2021-09-25 11:12:02 +0100

    Update via npm + remove currently unneeded components

--- assets/ts/index.ts
+++ assets/ts/index.ts
@@ -1,15 +1,3 @@
 import '../scss/index.scss';
 
-const contactContainer = document.getElementById('js-contact') as HTMLDivElement|null;
-if (contactContainer !== null) {
-    import(/* webpackChunkName: "contact" */'./contact/index').then(module => {
-        module.default(contactContainer);
-    });
-}
-
-const hcaptcha = document.querySelector('.h-captcha') as HTMLFormElement|null;
-if (hcaptcha !== null) {
-    import(/* webpackChunkName: "contact-lite" */'./hcaptcha/index').then(module => {
-        module.default();
-    });
-}
+console.log('hello');
